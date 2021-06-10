4d1d31a1d9ae891b309e2ed8fbf5fe88abcfbe07
Hassan <syed.hassan.ibrahim53@gmail.com>
2021-06-11 00:06:35 +0500

    Second Commit

--- app/src/main/java/com/hassan/firebaseapp/MainActivity.java
+++ app/src/main/java/com/hassan/firebaseapp/MainActivity.java
@@ -1,30 +1,71 @@
 package com.hassan.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    EditText et;
+    EditText title, genre;
     Button bt;
+    Button read;
+    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et = findViewById(R.id.data);
+        title = findViewById(R.id.editTitle);
+        genre = findViewById(R.id.editGenre);
         bt = findViewById(R.id.button);
+        read = findViewById(R.id.extract);
+        tx = findViewById(R.id.Rdata);
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase.getInstance().getReference("somedata").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        tx.setText(snapshot.getValue().toString());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String data = et.getText().toString();
-                FirebaseDatabase.getInstance().getReference("somedata").setValue(data);
+                String sentTitle = title.getText().toString();
+                String sentgenre = genre.getText().toString();
+                FirebaseDatabase.getInstance().getReference("movies").push().setValue(new Movie(sentTitle,sentgenre)).addOnCompleteListener(new OnCompleteListener<Void>(){
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Successfully inserted", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "UnSuccessfully!!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
---
+++ app/src/main/java/com/hassan/firebaseapp/Movie.java
@@ -0,0 +1,27 @@
+package com.hassan.firebaseapp;
+
+public class Movie {
+    String title;
+    String genre;
+
+    public Movie(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
