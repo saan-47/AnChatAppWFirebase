b29212daa99f6674c31a19958d0fdb42f25905d4
Hassan <syed.hassan.ibrahim53@gmail.com>
2021-06-16 22:38:51 +0500

    fifth Commit

---
+++ app/src/main/java/com/hassan/firebaseapp/Chathere.java
@@ -0,0 +1,14 @@
+package com.hassan.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chathere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chathere);
+    }
+}
